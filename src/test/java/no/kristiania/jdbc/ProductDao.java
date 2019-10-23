f87a9756a1fafa784646d37a6ce2e3e9bd9c1eef
Daniel <daniel.rm99@hotmail.com>
2019-10-23 17:12:28 +0200

    test1

---
+++ src/test/java/no/kristiania/jdbc/ProductDao.java
@@ -0,0 +1,6 @@
+package no.kristiania.jdbc;
+
+public class ProductDao {
+    public void insertProduct(java.lang.String rolex) {
+    }
+}
--- src/test/java/no/kristiania/jdbc/Rolex.java
+++ src/test/java/no/kristiania/jdbc/Rolex.java
@@ -1,4 +1,17 @@
 package no.kristiania.jdbc;
 
+import org.junit.jupiter.api.Test;
+
 public class Rolex {
+
+@Test
+
+
+void shouldRetrieveStoredProduct() {
+
+    ProductDao dao = new ProductDao();
+    dao.insertProduct("Rolex");
+    assertThat(dao.listAll()).contains("Rolex");
+
+}
 }
