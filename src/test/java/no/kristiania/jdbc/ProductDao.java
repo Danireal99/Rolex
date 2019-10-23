fec5a05668901e7c1d0388c90653918fd74bc7b5
André Stokkeland <andrestokk@hotmail.no>
2019-10-23 19:53:56 +0200

    connection please have a look at it

--- src/test/java/no/kristiania/jdbc/ProductDao.java
+++ src/test/java/no/kristiania/jdbc/ProductDao.java
@@ -1,5 +1,8 @@
 package no.kristiania.jdbc;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -7,10 +10,17 @@ import java.util.List;
 public class ProductDao {
 
     private List<String> products = new ArrayList<>();
+    private DataSource dataSource;
 
     public void insertProduct(String ProductName) {
    products.add(ProductName);
+
+    Connection conn;
+    PreparedStatement statement = conn.prepareStatement(sql:"insert into products (name) value (?)");
+    statement.setString(parameterIndex: 1, ProductName);
+    statement.executeUpdate();
     }
+
 
 
 
